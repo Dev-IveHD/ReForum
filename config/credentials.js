89f6ba12a7448f868dca8d6ece8007a168cce8c7
Iven de Soost <angi@bka.de>
2018-07-20 22:05:19 +0200

    ASdasda_:

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
-  GITHUB_CALLBACK_URL: '',
-  DBURL: '',
+  GITHUB_CLIENT_ID: '0495fccffecbbc231a45',
+  GITHUB_CLIENT_SECRET: '008f2cc92260bbf0f9cdf71d84e0c2e251616afb',
+  GITHUB_CALLBACK_URL: 'https://localhost:8080/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017',
 };
